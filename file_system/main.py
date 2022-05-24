16c1d66422eec2460ce44b28455249124cf7e5df
Zharkyn20 <61348606+Zharkyn20@users.noreply.github.com>
2022-05-24 20:01:55 +0600

    Remove useless code

--- file_system/main.py
+++ file_system/main.py
@@ -1,9 +1,5 @@
 from commands import *
 
 _, command, *args = sys.argv
-
-if command in commands_dict.keys():
-    check_commands(sys.argv)
-    commands_dict[command](*args)
-else:
-    help_me()
+check_commands(sys.argv)
+commands_dict[command](*args)
